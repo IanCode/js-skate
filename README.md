0b80f497140f23ac0f922707d13811439ba895f4
Ian <iwwhite@pugetsound.edu>
2017-10-23 09:35:05 -0700

    readme updates

---
+++ README.md
@@ -0,0 +1,7 @@
+#SpotFinder#
+##An app to help you shred!##
+This is a web app using JS, JSON, and MongoDB to bring you local spots nearby! The site is
+hosted on AWS and offers users the opportunity to find skate spots near them. Whether you're
+looking for a skatepark, a ledge, stairset, handrail, or even just a good hill to bomb,
+spotfinder takes input from the local skater community and displays all the good spots in town so
+you know where to go. 
--- app/index.js
+++ app/index.js
@@ -9,6 +9,19 @@ const users = []
 
 const app = express()
 
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017';
+var bodyParser = require('body-parser');
+
+
+MongoClient.connect(url, function(err, db) {
+	assert.equal(null, err);
+	console.log("Connected correctly to server");
+	db.close();
+});
+
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/'));
 
 app.engine('.hbs', exphbs({
@@ -33,26 +46,37 @@ app.use((err, request, response, next) => {
 //storing user data in a global variable
 ////issues include limited RAM, memory resets with application,
 ////can result in stack overflow
-app.post('/users', function (req, res) {
+/*app.post('/users', function (req, res) {
   const user = req.body
   users.push({
     name: user.name,
     age: user.age
   })
   res.send('successfully registered')
-})
+})*/
 
 //storing data in a file
 ////scaling issues, updating has problems (locks), 
 ///
-const fs = require('fs')
+/*const fs = require('fs')
 app.post('/users', function (req, res) {
   const user = req.body
   fs.appendFile('users.txt', JSON.stringify({ name: user.name, 
     age: user.age }), (err) => {
     res.send('successfully registered')
   })
-})
+})*/
+
+//storing data directly in mongodb (is this the best way?)
+app.post('/', function (req, res) {
+	db.collection('users').insert(req.body, function (err, result) {
+		if (err)
+			res.send('Error');
+		else
+			res.send('Success');
+	});
+});
+
 
 
 app.get('/new_spot', (request, response) => {
@@ -63,12 +87,18 @@ app.get('/signup', (request, response) => {
   response.render('signup')
 })
 
+app.get('/gametime1999', (request, response) => {
+  response.render('gametime1999')
+})
+
 app.get('/', (request, response) => {
   response.render('home', {
     name: 'John'
   })
 })
 
-
+app.get('/about', (request, response) => {
+  response.render('about')
+})
 
 app.listen(3000)
